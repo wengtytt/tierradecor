70b06fd4b4f6eabb8e51c734f4488dbacfe499d5
tianyu <tianyu@proximify.ca>
2020-05-19 21:56:54 -0400

    Updates

---
+++ precache-manifest.b12414cf9d1d2ba8fc76bdeb1408ba6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4a4f28fbb8d6647cf135091fbb022d7",
+    "url": "/tierradecor/index.html"
+  },
+  {
+    "revision": "5bbc6f2b3900391c4efc",
+    "url": "/tierradecor/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6f87e767cd1299e003cc",
+    "url": "/tierradecor/static/js/2.69b03fcd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tierradecor/static/js/2.69b03fcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bbc6f2b3900391c4efc",
+    "url": "/tierradecor/static/js/main.a6f2906a.chunk.js"
+  },
+  {
+    "revision": "337aae5084495adbaefe",
+    "url": "/tierradecor/static/js/runtime-main.6402f287.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tierradecor/static/media/logo.5d5d9eef.svg"
+  }
+]);
