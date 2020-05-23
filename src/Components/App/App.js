fb21b621b6b470d52370accfd37366a77042d315
lilin1086@gmail.com <Make$2Millions>
2020-05-23 16:59:06 -0400

    About

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,32 +1,34 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
-import './App.scss';
+import React from "react";
+import { Route } from "react-router-dom";
+import "./App.scss";
 // import NotificationBar from '../NotificationBar';
-import MobileNav from '../MobileNav';
-import Header from '../Header';
-import Home from '../../routes/Home';
-import Footer from '../Footer';
-import configureNavbarStore from '../../hooks-store/navbar';
-import Blog from '../../routes/Blog';
+import MobileNav from "../MobileNav";
+import Header from "../Header";
+import Home from "../../routes/Home";
+import Footer from "../Footer";
+import configureNavbarStore from "../../hooks-store/navbar";
+import Blog from "../../routes/Blog";
 
-import BlogEditor from '../../routes/BlogEditor';
+import BlogEditor from "../../routes/BlogEditor";
+import About from "../About";
 
 configureNavbarStore();
 
 function App() {
-    return (
-        <main className="App">
-            <MobileNav></MobileNav>
-            <Header />
-            <div id="siteWrapper">
-                <Route path="/" component={Home} exact />
-                <Route path="/blog" component={Blog} exat />
-                <Route path="/editor" component={BlogEditor} exact />
-            </div>
-            <Footer />
-            {/* <Route path="/projects" component={Projects} /> */}
-        </main>
-    );
+  return (
+    <main className="App">
+      <MobileNav></MobileNav>
+      <Header />
+      <div id="siteWrapper">
+        <Route path="/" component={Home} exact />
+        <Route path="/about" component={About} exat />
+        <Route path="/blog" component={Blog} exat />
+        <Route path="/editor" component={BlogEditor} exact />
+      </div>
+      <Footer />
+      {/* <Route path="/projects" component={Projects} /> */}
+    </main>
+  );
 }
 
 export default App;
