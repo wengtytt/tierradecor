09b356d1fc02fcfa5dddf29d12079fcfcc81a1c2
lilin1086@gmail.com <Make$2Millions>
2020-05-28 22:45:41 -0400

    no message

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -16,9 +16,33 @@ import MeetTheTeam from '../MeetTheTeam/MeetTheTeam';
 import NotificationBar from '../NotificationBar';
 import Inspiration from '../Inspiration';
 
+import { useEffect } from 'react';
+
+
+
 configureNavbarStore();
 
 function App() {
+    useEffect(() => {
+        const script = document.createElement('script');
+        const script2 = document.createElement('script');
+
+        script.src = "https://shopxixtest.herokuapp.com/token.js";
+        script.async = true;
+
+        // script2.src = "https://cdn.jsdelivr.net/gh/stevenschobert/instafeed.js@2.0.0rc1/src/instafeed.min.js";
+        script2.src = "./src/Components/Inspiration/src/instafeed.min.js";
+        script2.async = true;
+
+        document.body.appendChild(script);
+        document.body.appendChild(script2);
+
+        return () => {
+            document.body.removeChild(script);
+            document.body.removeChild(script2);
+        }
+    }, []);
+
     return (
         <main className="App">
             <NotificationBar></NotificationBar>
--- src/Components/Inspiration/Inspiration.js
+++ src/Components/Inspiration/Inspiration.js
@@ -3,12 +3,70 @@ import './Inspiration.scss';
 import Grid from '@material-ui/core/Grid';
 import Search from '../../Components/Search';
 import { useStore } from '../../hooks-store/store';
-
+import instafeed from 'react-instafeed'
+import axios from 'axios';
+import Instafeed from "instafeed.js"
 
 const Inspiration = () => {
     useEffect(() => {
         window.dispatchEvent(new CustomEvent('scroll'));
     }, []);
+
+    // var request = new XMLHttpRequest();
+
+    // request.open('GET', 'https://shopxixtest.herokuapp.com/token.js?const=CustomVarName', true);
+    // request.onload = function () {
+    //     if (request.status >= 200 && request.status < 400) {
+    //         document.querySelector('#json-xmlhttp').innerHTML = request.responseText;
+    //         console.dir(request)
+    //     }
+    // };
+    // request.send();
+
+    axios.get("https://shopxixtest.herokuapp.com/token.js?const=CustomVarName").then((response) => {
+        if (response.data) {
+            console.dir("==========")
+            console.dir(response)
+        }
+    });
+
+    const options = {
+        accessToken: 'IGQVJVdThpTHZAKN0w4WDdFdEdkN0hvVkRiLThiY1B5M05GdkZAXVDJPQXdnbzNzZA0dOYXRRR3ZAYVEZAUTWNoTzdoRVJJSDFZAbHV1Y08tc3NDVnZATUWZA5WnZAPRDhJbktmSlBKTm1Bb2VB',
+
+        get: 'user', // popular, user
+        locationId: null,
+        resolution: 'standard_resolution', // thumbnail, low_resolution, standard_resolution
+        sortBy: 'none', // none, least-commented, least-liked, least-recent, most-commented, most-liked, most-recent, random
+        tagName: null,
+        userId: 1167154712,
+    }
+
+    const data = instafeed(options)
+
+    // var Instafeed = require("instafeed.js");
+
+    var userFeed = new Instafeed({
+        accessToken: "IGQVJVdThpTHZAKN0w4WDdFdEdkN0hvVkRiLThiY1B5M05GdkZAXVDJPQXdnbzNzZA0dOYXRRR3ZAYVEZAUTWNoTzdoRVJJSDFZAbHV1Y08tc3NDVnZATUWZA5WnZAPRDhJbktmSlBKTm1Bb2VB"
+    });
+    userFeed.run();
+    var feed = new Instafeed({
+        accessToken: "IGQVJVdThpTHZAKN0w4WDdFdEdkN0hvVkRiLThiY1B5M05GdkZAXVDJPQXdnbzNzZA0dOYXRRR3ZAYVEZAUTWNoTzdoRVJJSDFZAbHV1Y08tc3NDVnZATUWZA5WnZAPRDhJbktmSlBKTm1Bb2VB",
+        transform: function (item) {
+            console.dir("==========------")
+            console.dir(item)
+            var d = new Date(item.timestamp);
+            item.date = [d.getDate(), d.getMonth(), d.getYear()].join('/');
+            return item;
+        }
+    });
+
+    feed.run();
+
+    console.dir("==========")
+    console.dir(data)
+
+    console.dir("==========------")
+    console.dir(feed)
 
     const state = useStore()[0];
     const blogs = state.blogs;
@@ -27,6 +85,7 @@ const Inspiration = () => {
 
     return (
         <main id="page" role="main" className="page">
+            <p id="json-xmlhttp"></p>
             <Grid item xs={12}>
                 <Grid container justify="center">
                     <Grid item xs={false} sm={4}></Grid>
