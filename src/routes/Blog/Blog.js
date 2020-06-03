4a9348bc73a55e21c4a9dc1057a03f7f2324e58f
lilin1086@gmail.com <Make$2Millions>
2020-06-02 21:17:18 -0400

    blog1

--- src/routes/Blog/Blog.js
+++ src/routes/Blog/Blog.js
@@ -20,31 +20,38 @@ const Blog = () => {
                     <img src={img} alt={item.tags}></img>
                 </div>
                 <h2 id={item.id}>{item.tags}</h2>
+                <div className="author">CANDACE PLOTZ - MAY 29, 2020</div>
+                <p>I often hear from clients that they’re not sure what to do with their mantel - so I want to share some Do’s and Don’ts on how to decorate. I want this to be fun and I hope these tips help you get off to the right start.</p>
+                <span>Read More →</span>
             </Grid>
         );
     });
 
     return (
-        <main id="page" role="main" className="page">
-            <Grid item xs={12}>
-                <Grid container justify="center">
-                    <Grid item xs={false} sm={4}></Grid>
-                    <Grid item xs={12} sm={4} className="blog-title">
-                        <h1 style={{ textAlign: 'center' }}>BLOG</h1>
-                    </Grid>
-                    <Grid item xs={12} sm={4}>
-                        <Search></Search>
-                    </Grid>
+        <main id="page" role="main" className="blog">
+            <section className="section-wrapper">
+                <div className="h1-block">
+                    <h1
+                        style={{
+                            textAlign: 'center',
+                            whiteSpace: 'pre-wrap',
+                        }}>
+                        Blog
+                </h1>
+                </div>
+                <div className="search"><Search></Search></div>
+            </section>
+
+            <section className="section-wrapper">
+                <Grid
+                    container
+                    className="blog-list"
+                    spacing={3}
+                    justify="flex-start"
+                >
+                    {markup}
                 </Grid>
-            </Grid>
-            <Grid
-                container
-                className="blog-list"
-                spacing={3}
-                justify="flex-start"
-            >
-                {markup}
-            </Grid>
+            </section>
         </main>
     );
 };
